f3225a7f000b4efeeddca6ce288a1ce236fb6a93
adkrueger <adkrueger@wpi.edu>
2019-07-23 14:46:40 -0400

    Initial commit

---
+++ microblog.py
@@ -0,0 +1,1 @@
+from pyapp import app
---
+++ pyapp/routes.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from pyapp import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Aaron'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Lovin\' me some tasty boigas!'
+        },
+        {
+            'author': {'username': 'Barbara'},
+            'body': 'I\'m vegan and I hate boigas!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
